54b3a96a5a830793f841e8694f1067afdaa5bf2c
MahyarNV <behzadimahyar85@gmail.com>
2021-03-20 01:52:34 +0330

    add, fetch (all and one) queries to class

--- src/ctpysql.py
+++ src/ctpysql.py
@@ -1,18 +1,21 @@
+from mysql.connector import (Error)
+
 class ctpysql:
     def __init__(self, conn):
         self.conn = conn
+        self.Error = Error
 
     def truncate (self, table):
         sql = "truncate {0}".format(table)
         try:
             cursor = self.conn.cursor()
             cursor.execute(sql)
+            cursor.close()
             return self.conn.commit()
 
         except Error as e:
             return e
 
-        cursor.close()
 
     def insert (self, table, dic):
         columns = ', '.join("`" + str(i).replace('/', '_') + "`" for i in dic.keys())
@@ -21,9 +24,32 @@ class ctpysql:
         try:
             cursor = self.conn.cursor()
             cursor.execute(sql)
+            cursor.close()
             return self.conn.commit()
 
         except Error as e:
             return e
 
-        cursor.close()
+    def fetch(self, table, case, dic):
+        condition = ""
+        for i in dic:
+            if condition != "":
+                condition += " AND {0} = {1}".format(i, dic[i])
+
+            else:
+                condition += "{0} = {1}".format(i, dic[i])
+
+        sql = "SELECT * FROM {0} WHERE {1}".format(table, condition)
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+            if case == 'all':
+                row = cursor.fetchall()
+
+            elif case == 'one':
+                row = cursor.fetchone()
+            cursor.close()
+            return row
+
+        except Error as e:
+            return e
