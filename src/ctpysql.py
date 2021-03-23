7bd345890e51812fa44d0a47db75b7fb13077549
MahyarNV <behzadimahyar85@gmail.com>
2021-03-24 00:24:08 +0430

    add update and delete(simple) queries

--- src/ctpysql.py
+++ src/ctpysql.py
@@ -10,8 +10,9 @@ class ctpysql:
         try:
             cursor = self.conn.cursor()
             cursor.execute(sql)
+            self.conn.commit()
             cursor.close()
-            return self.conn.commit()
+            return "Success"
 
         except Error as e:
             return e
@@ -24,8 +25,9 @@ class ctpysql:
         try:
             cursor = self.conn.cursor()
             cursor.execute(sql)
+            self.conn.commit()
+            return cursor.lastrowid
             cursor.close()
-            return self.conn.commit()
 
         except Error as e:
             return e
@@ -54,6 +56,39 @@ class ctpysql:
                 row = cursor.fetchone()
             cursor.close()
             return row
+
+        except Error as e:
+            return e
+
+    def update(self, table, condition, condition_value, **data):
+        sql = "UPDATE {0} SET ".format(table)
+        i = 0
+        for key, value in data.items():
+            sql += "{0} = {1}".format(key, value)
+            if i < len(data.items())-1:
+                sql += ", "
+            i += 1
+
+        sql += " WHERE {0} = {1}".format(condition, condition_value)
+
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+            self.conn.commit()
+            return cursor.rowcount
+            cursor.close()
+
+        except Error as e:
+            return e
+
+    def delete(self, table, condition, condition_value):
+        sql = "DELETE FROM {0} WHERE {1} = {2} ".format(table, condition, condition_value)
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+            self.conn.commit()
+            return cursor.rowcount
+            cursor.close()
 
         except Error as e:
             return e
