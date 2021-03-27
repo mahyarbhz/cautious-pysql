9d9a0a4a05b2ef91cadba9847e52e41a9ad55a3b
MahyarNV <behzadimahyar85@gmail.com>
2021-03-27 23:31:26 +0430

    update setup.py to new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ctpysql',
-    version='1.3.6',
+    version='1.4.0',
     description='This library helps you run MYSQL queries as fast as possible 🚄🔥!',
     py_modules=["ctpysql"],
     package_dir={'': 'src'},
